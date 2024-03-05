33c5beeac275bd75a66d9ffd80b2d1481fd4ca69
nadorator <nhmerouane@hotmail.com>
2024-03-05 21:56:17 +0100

    fix guild member detection

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,35 @@
 const { createTicket } = require('../database/queries/ticket');
+const { ChannelType } = require('discord.js');
 
 module.exports = async (client, member) => {
-	console.log(member);
-	await createTicket(
-		member.user.id,
-		member.user.username,
-		member.user.createdTimestamp,
-		member.user.tag,
-	);
-	console.log('ticket created for ' + member.user.username);
+	// check if user is a real user then
+	if (!member.user.bot && !member.user.system) {
+		console.log(member);
+		// store user in database
+		await createTicket(
+			member.user.id,
+			member.user.username,
+			member.user.createdTimestamp,
+			member.user.nickname,
+		);
+
+		console.log('ticket created for ' + member.user.username);
+		// create new private channel
+		// move user to channel with welcome message
+		// send pm to receptionists group
+		await member.guild.channels.create('ticket-' + member.user.id, {
+			// permissionOverwrites: [
+			// 	{
+			// 		id: everyone.id,
+			// 		deny: 'VIEW_CHANNEL',
+			// 	}, {
+			// 		id: support.id,
+			// 		allow: 'VIEW_CHANNEL',
+			// 	}, {
+			// 		id: message.author.id,
+			// 		allow: 'VIEW_CHANNEL',
+			// 	},
+			// ],
+		});
+	}
 };
