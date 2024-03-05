f6e055fa0a62fb4c8dab43f7a12c60d821f1eff0
nadorator <twins20@twins20.com>
2024-03-05 09:06:31 +0100

    modified:   database/queries/ticket.js 	modified:   events/guildMemberAdd.js

--- database/queries/ticket.js
+++ database/queries/ticket.js
@@ -1,6 +1,6 @@
 const TicketSchema = require('../schema/ticket');
 
-const createTicket = async (userId, username, timeStamp,tag) => {
+const createTicket = async (userId, username, timeStamp, tag) => {
 	const query = new TicketSchema({
 		user: {
 			id: userId,
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,12 +2,11 @@ const { createTicket } = require('../database/queries/ticket');
 
 module.exports = async (client, member) => {
 	console.log(member);
-			await createTicket(
-				member.user.id,
-				member.user.username,
-				member.user.createdTimestamp,
-				member.user.tag,
-			);
-	console.log('ticket created for ' + member.user.username);	
-	}
+	await createTicket(
+		member.user.id,
+		member.user.username,
+		member.user.createdTimestamp,
+		member.user.tag,
+	);
+	console.log('ticket created for ' + member.user.username);
 };
