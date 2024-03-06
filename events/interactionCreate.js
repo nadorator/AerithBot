ee8609272d9474128458328ac8a3d28c546edc36
nadorator <nhmerouane@hotmail.com>
2024-03-07 00:26:38 +0100

    fix guild member detection

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -10,11 +10,13 @@ async function run(client, interaction) {
 
 	try {
 		await command.execute(interaction);
-	} catch (error) {
+	}
+	catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
 			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
+		}
+		else {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
